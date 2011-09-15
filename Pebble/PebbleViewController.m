a99094d07e0bf8b8f6ee16103047ed328fed0360
Jon Fleming <jonfleming@hotmail.com>
2011-09-15 09:08:44 -0700

    initial commit

--- Pebble/PebbleViewController.m
+++ Pebble/PebbleViewController.m
@@ -74,12 +74,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
+    return YES;
 }
 
 @end
